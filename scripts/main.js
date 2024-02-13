52dfbc98ac93244fb6d5866604360f58f294be52
diegoespin <andres_diego_espin@hotmail.com>
2024-02-13 08:49:31 +0100

    fin

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,7 @@
-console.log("test");
+let maCouleur = document.querySelector("input").value
+let monBouton = document.querySelector("button")
+let monBody = document.querySelector("body")
+
+monBouton.addEventListener("click", function(){
+    monBody.style.background = maCouleur  
+})
